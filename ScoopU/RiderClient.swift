43b275e0242747457b1def294e97437c99bcd5c0
Taylor Ferrari <taylorferrari@taylors-mbp.hws.edu>
2016-12-04 18:10:21 -0500

    initial commit

---
+++ ScoopU/RiderClient.swift
@@ -0,0 +1,9 @@
+//
+//  RiderClient.swift
+//  ScoopU
+//
+//  Created by Taylor Ferrari on 12/4/16.
+//  Copyright © 2016 ScoopU. All rights reserved.
+//
+
+import Foundation
---
+++ ScoopU/Server.swift
@@ -0,0 +1,9 @@
+//
+//  Server.swift
+//  ScoopU
+//
+//  Created by Taylor Ferrari on 12/4/16.
+//  Copyright © 2016 ScoopU. All rights reserved.
+//
+
+import Foundation

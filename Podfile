d8c08266d3b14810995ab12780f787faaf356794
Krista Anken <kristaanken@kristas-mbp.hws.edu>
2016-12-07 22:16:29 -0500

    added a database and code

---
+++ Podfile
@@ -0,0 +1,9 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ScoopU' do
+
+pod ‘Firebase’
+pod ‘Firebase/Auth’
+ 
+end
--- ScoopU/ViewController.swift
+++ ScoopU/ViewController.swift
@@ -8,7 +8,13 @@
 
 import UIKit
 
+import Firebase
+
 class ViewController: UIViewController {
+    
+    @IBOutlet var email: UITextField!
+    
+    @IBOutlet var password: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +25,79 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //called by the register button - creates an account in the Firebase database
+    @IBAction func createAccount(_ sender: Any) {
+
+        FIRAuth.auth()?.createUser(withEmail: email.text!, password: password.text!, completion: {
+            
+            user, error in
+            
+            //if there was no error, call login
+            
+            if error != nil {
+     
+                self.login()
+ 
+                
+            } else {
+
+                //everything went smoothly
+                
+                print("user created")
+                
+                self.login()
+            }
+            
+
+        })
+
+        
+    }
+
+    //function called when log In button is pressed
+    
+    @IBAction func logInUser(_ sender: Any) {
+         self.login()
+    }
+   
+    
+    //function that is called when the user logs in - if the email/password is incorrect, print error
+    
+    func login() {
+        
+        FIRAuth.auth()?.signIn(withEmail: email.text!, password: password.text!, completion: {
+            
+            user, error in
+            
+            if error != nil{
+                
+                print("password / username is incorrect")
+                
+            } else {
+                
+                print("Huzzah!")
+                
+                self.performSegue(withIdentifier: "Driver or Rider", sender: self)
+                
+                
 
 
+            }
+
+        })
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 }
-
